3ea559fc02539eb8fc1c922a433e52faf36e5d08
Cadnuns Dimir <cadnuns.dimir@gmail.com>
2020-10-13 21:10:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var $paragraph = document.querySelector('p')
 
 $paragraph.textContent = 'Conteúdo inserido via javascript'
+
+function newEl(tag, content) {
+  var $el = document.createElement(tag);
+  $el.innerHTML = content;
+  return $el;
+}
+
